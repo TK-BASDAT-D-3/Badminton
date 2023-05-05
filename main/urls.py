60446b44dc56a7ab960e97ac2655452cfdeb6f04
risalstr <risa.lestari2002@gmail.com>
2023-05-06 01:33:03 +0700

    add login and register page

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_landing, name='show_landing'),
+    path('register/', show_register, name='show_register'),
+    path('login/', show_login, name='show_login'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, redirect
 
 def show_landing(request):
     return render(request, 'base.html', {})
+
+
+def show_register(request):
+    return render(request, 'register.html', {})
+
+
+def show_login(request):
+    return render(request, 'login.html', {})

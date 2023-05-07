d69a3e82d745253d88ffdb68c408728bac3d3f4f
risalstr <risa.lestari2002@gmail.com>
2023-05-07 21:06:43 +0700

    kay task

--- main/views.py
+++ main/views.py
@@ -15,3 +15,31 @@ def show_login(request):
 
 def show_pertandingan(request):
     return render(request, 'pertandingan.html', {})
+
+
+def daftar_atlet(request):
+    return render(request, "daftar_atlet.html")
+
+
+def show_list_atlet(request):
+    return render(request, "list_atlet.html")
+
+
+def show_dashboard_atlet(request):
+    return render(request, "dashboard_atlet.html")
+
+
+def show_dashboard_pelatih(request):
+    return render(request, "dashboard_pelatih.html")
+
+
+def show_dashboard_umpire(request):
+    return render(request, "dashboard_umpire.html")
+
+
+def tes_kualifikasi_atlet(request):
+    return render(request, "tes_kualifikasi_atlet.html")
+
+
+def isi_tes_kualifikasi_atlet(request):
+    return render(request, "isi_tes_kualifikasi_atlet.html")

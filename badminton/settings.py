d030cb7ec81b09cd73521727db5e9f3cfda93e38
risalstr <risa.lestari2002@gmail.com>
2023-05-07 21:44:43 +0700

    fix bug

--- badminton/settings.py
+++ badminton/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "atlet"
+    "atlet",
+    "main",
+    "authentication"
 ]
 
 MIDDLEWARE = [
@@ -48,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'  # additional
 ]
 
 ROOT_URLCONF = 'badminton.urls'

f5560c5d44dba5ce3e05eeb5622e9bea1debee6a
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-05-16 11:53:49 +0700

    change database to postgree

--- badminton/settings.py
+++ badminton/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'badminton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'badminton',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

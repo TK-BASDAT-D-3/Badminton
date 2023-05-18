edbf17cefe581b535318d9b38d2bcdc5cd0a7476
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-05-18 10:24:40 +0700

    trigger update the world rank in atlet_kualifikasi

--- triggers/trigger update world rank.sql
+++ triggers/trigger update world rank.sql
@@ -15,6 +15,20 @@ BEGIN
     FROM ranked
     WHERE ATLET.ID = ranked.ID;
 
+    -- Calculate new ranks
+    WITH ranked AS (
+        SELECT ID, ROW_NUMBER() OVER (ORDER BY sum_points DESC) AS rank
+        FROM (
+            SELECT ID_Atlet AS ID, SUM(Total_Point) as sum_points
+            FROM badudu.POINT_HISTORY
+            WHERE Tahun = EXTRACT(YEAR FROM CURRENT_DATE)
+            GROUP BY ID_Atlet
+        ) as subquery
+    )
+    UPDATE badudu.ATLET_KUALIFIKASI SET World_Rank = ranked.rank
+    FROM ranked
+    WHERE ATLET_KUALIFIKASI.ID_Atlet = ranked.ID;
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
